33412b7338aebfd153f48117ace8384d181b5ded
Dhruvi <dhruvihl369@gmail.com>
2024-08-13 17:27:43 -0400

    added custom array

--- customArray.js
+++ customArray.js
@@ -17,6 +17,23 @@ class MyArray {
     delete this.data[this.length - 1];
     this.length--;
   }
+
+  shift() {
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+  delete(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
 
 const myNewArray = new MyArray();
@@ -30,7 +47,13 @@ myNewArray.push("Grape");
 // get specific array
 console.log(myNewArray.get(3));
 
-// delete last array item
-myNewArray.pop();
+// // delete last array item
+// myNewArray.pop();
+
+// // shift data by 1 from front
+// myNewArray.shift();
+
+// delete item by index
+myNewArray.delete(2);
 
 console.log(myNewArray);
